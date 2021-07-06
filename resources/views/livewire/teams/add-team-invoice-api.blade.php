e01fbad9962fbd06326382fce04b12331dcee477
harryheslin <hheslin@gmail.com>
2021-07-06 15:24:47 +1000

    CSS Foundations for team API settings Migration written for token storage and invoice APIs

---
+++ resources/views/livewire/teams/add-team-invoice-api.blade.php
@@ -0,0 +1,22 @@
+<x-jet-action-section>
+    <div class="mt-10 sm:mt-0">
+        <x-slot name="title">
+            {{ __('Add New Invoice Services') }}
+        </x-slot>
+        <x-slot name="description">
+            {{ __('Add additonal services that are supported through Earn.') }}
+        </x-slot>  
+        <x-slot name="content">
+            <div class="grid grid-cols-6">
+                @foreach ($services as $service)
+                    <div class="col-start-1 col-span-3">
+                        <x-jet-label value="{{ $service->name }}" />
+                    </div>
+                    <div class="col-start-5 col-span-2 mb-2">
+                        <x-jet-button wire:click="signin({{ $service->name }})"> {{ __('Sign In') }} </x-jet-button>  
+                    </div>
+                @endforeach
+            </div>
+        </x-slot>
+    </div>
+</x-jet-action-section>
