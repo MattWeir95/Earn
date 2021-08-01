22f59a9191f8c9c4bbba89acb4d1177be1e8a1db
harryheslin <hheslin@gmail.com>
2021-08-02 08:31:24 +1000

    Added Invoice Service Tests

--- tests/Feature/RegistrationTest.php
+++ tests/Feature/RegistrationTest.php
@@ -14,14 +14,14 @@ class RegistrationTest extends TestCase
     public function test_registration_screen_can_be_rendered()
     {
         $response = $this->get('/register');
-
         $response->assertStatus(200);
     }
 
     public function test_new_users_can_register()
     {
         $response = $this->post('/register', [
-            'name' => 'Test User',
+            'first_name' => 'test_first',
+            'last_name' => 'test_last',
             'email' => 'test@example.com',
             'password' => 'password',
             'password_confirmation' => 'password',
