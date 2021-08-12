7874524beeba35c5d5c224a3207d82e54782c528
Matt Weir <n10509020@qut.edu.au>
2021-08-12 21:31:44 +1000

    CSS Changes

--- resources/views/livewire/employees/home.blade.php
+++ resources/views/livewire/employees/home.blade.php
@@ -1,4 +1,4 @@
-<widget-container class="grid grid-cols-1 lg:grid-cols-2 gap-12 p-12">
+<widget-container class="grid grid-cols-1 lg:grid-cols-2 gap-12 lg:p-12 pt-12">
         @livewire('employees.guage', ['user' => Auth::user()])
         @livewire('employees.graph', ['user' => Auth::user()])
 </widget-container>
--- resources/views/rules.blade.php
+++ resources/views/rules.blade.php
@@ -4,7 +4,7 @@
     @endphp
     @if (Gate::check('isManager', $user->currentTeam))
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8" >
-            <widget-container class="grid grid-cols-1 lg:grid-cols-2 gap-12 p-12">
+            <widget-container class="grid grid-cols-1 lg:grid-cols-2 gap-12 lg:p-12 pt-12">
                 @livewire('managers.rules.view-rules-list' ,['team' => $user->currentTeam])
                 @livewire('managers.rules.edit-rules', ['team' => $user->currentTeam])
             </widget-container>
