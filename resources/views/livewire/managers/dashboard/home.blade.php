86925b448ca2120fc90a4a7754d805ddd914ea33
Matt Weir <n10509020@qut.edu.au>
2021-08-19 12:42:08 +1000

    Fixing spacing of components on mobile view

--- resources/views/livewire/managers/dashboard/home.blade.php
+++ resources/views/livewire/managers/dashboard/home.blade.php
@@ -1,13 +1,17 @@
 {{-- Manager Home Screen --}}
 @if ($total > 0)
-<widget-container class="grid grid-cols-1 lg:grid-cols-2 gap-12 p-12">
+<div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8" >
+<widget-container class="grid grid-cols-1 lg:grid-cols-2 gap-12 lg:p-12 pt-12">
     @livewire('managers.dashboard.employee-list', ['user' => Auth::user()])
     @livewire('managers.dashboard.outstanding-carousel', 
         ['user' => Auth::user()]
     )
 </widget-container>
+</div>
 @else
-<widget-container class="grid grid-cols-1 gap-12 p-12">
+<div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8" >
+<widget-container class="grid grid-cols-1 gap-12 lg:p-12 pt-12">
     @livewire('managers.dashboard.employee-list', ['user' => Auth::user()])
 </widget-container>
+</div>
 @endif
