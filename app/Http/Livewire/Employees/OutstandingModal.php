ddf2628359d17ba66953d1fd61cbd0485972165d
harryheslin <hheslin@gmail.com>
2021-09-28 18:13:05 +1000

    Employee Approval Tests & Bug Fixes Started Tests on employee approval model Big discovered where prompts would repeat when multiple Fixed with refactor and return single history each render rather then an array

--- app/Http/Livewire/Employees/OutstandingModal.php
+++ app/Http/Livewire/Employees/OutstandingModal.php
@@ -29,12 +29,14 @@ class OutstandingModal extends Component
         $this->user = $user;
     }
 
+    //Approval Function used for single approval
     public function approve($id)
     {
         $com = new TeamCommission;
         $com->approveCommission($id);
     }
 
+    //Flag Function used for single approval
     public function flag($id)
     {
         $com = new TeamCommission;
@@ -43,31 +45,27 @@ class OutstandingModal extends Component
 
     public function render()
     {
-        $teamUser = TeamUser::where('team_id', $this->user->currentTeam->id)->where('user_id', $this->user->id)->first();
         $comOustanding = $this->user->historiesForTeam($this->user->currentTeam)
             ->where('flagged', 0)
             ->where('approved', 0)
-            ->get();
+            ->orderBy('end_time', 'asc')
+            ->first();
 
-
+        $dueApproval = array();
         if ($comOustanding != null) {
-            $dueApproval = array();
-            foreach ($comOustanding as $x) {
-                if (Carbon::parse($x->start_time)->format('m.y') != Carbon::today()->format('m.y') && $x->total_commission > 0) {
-                    array_push($dueApproval, [
-                        'id' => $x->id,
-                        'total' => $x->total_commission,
-                        'start_time' => Carbon::parse($x->start_time)->toFormattedDateString(),
-                        'end_time' => Carbon::parse($x->end_time)->toFormattedDateString(),
-                    ]);
-                }
+            if (Carbon::parse($comOustanding->start_time)->format('m.y') != Carbon::today()->format('m.y') && $comOustanding->total_commission > 0) {
+                array_push($dueApproval, [
+                    'id' => $comOustanding->id,
+                    'total' => $comOustanding->total_commission,
+                    'start_time' => Carbon::parse($comOustanding->start_time)->toFormattedDateString(),
+                    'end_time' => Carbon::parse($comOustanding->end_time)->toFormattedDateString()
+                ]);
+
+                return view('livewire.employees.outstanding-modal', [
+                    'approval' => $dueApproval
+                ]);
             }
         }
-        if (count($dueApproval) > 0) {
-            return view('livewire.employees.outstanding-modal', [
-                'dueApproval' => $dueApproval
-            ]);  
-        }
-        return ("<div></div>");
+        return ('<div id="no-approvals"></div>');
     }
 }
--- resources/views/livewire/employees/outstanding-modal.blade.php
+++ resources/views/livewire/employees/outstanding-modal.blade.php
@@ -1,28 +1,26 @@
-@foreach ($dueApproval as $approval)
-    <div x-data="{isOpen: false}" x-cloak x-show="isOpen" @keydown.escape.window="isOpen = false"
-        class="absolute inset-10 z-10 flex items-center justify-center" x-transition:enter="ease-out duration-300"
-        x-transition:enter-start="opacity-0 scale-90" x-transition:enter-end="opacity-100 scale-100"
-        x-transition:leave="transition ease-in duration-300" x-transition:leave-start="opacity-100 transform scale-100"
-        x-transition:leave-end="opacity-0 transform scale-90" x-init="setTimeout(() => isOpen = true, 1000)">
-        <div class="bg-white border-2 border-gray-200 rounded p-2">
-            <div @click="isOpen= false" class="flex flex-row justify-between">
-                <h1 class="font-bold text-2xl">Commission Approval</h1>
-                <button>X</button>
-            </div>
-            <div>
-                <p class="text-md mt-6">Your previous commission period is pending approval</p>
-                <p class="text-xl mt-6">Total: ${{ $approval['total'] }}</p>
-                <p class="text-md mt-2">Active Period: {{ $approval['start_time']}}-{{ $approval['end_time'] }}</p>
-            </div>
+<div x-data="{isOpen: false}" id="approval-modal" x-cloak x-show="isOpen" @keydown.escape.window="isOpen = false"
+    class="absolute inset-10 z-10 flex items-center justify-center" x-transition:enter="ease-out duration-300"
+    x-transition:enter-start="opacity-0 scale-90" x-transition:enter-end="opacity-100 scale-100"
+    x-transition:leave="transition ease-in duration-300" x-transition:leave-start="opacity-100 transform scale-100"
+    x-transition:leave-end="opacity-0 transform scale-90" x-init="setTimeout(() => isOpen = true, 1000)">
+    <div class="bg-white border-2 border-gray-200 rounded p-2">
+        <div @click="isOpen= false" class="flex flex-row justify-between">
+            <h1 class="font-bold text-2xl">Commission Approval</h1>
+            <button>X</button>
+        </div>
+        <div>
+            <p class="text-md mt-6">Your previous commission period is pending approval</p>
+            <p class="text-xl mt-6">Total: ${{ $approval[0]['total'] }}</p>
+            <p class="text-md mt-2">Active Period: {{ $approval[0]['start_time'] }}-{{ $approval[0]['end_time'] }}
+            </p>
+        </div>
 
-            <div class="flex justify-center pt-2 mt-6">
-                <button
-                    id={{ $approval['id'] }}
-                    class="px-4 bg-transparent p-2 rounded-lg border-2 border-indigo-500 text-indigo-500 hover:bg-gray-100 hover:text-indigo-400 mr-4 w-24"
-                    @click="$wire.approve({{ $approval['id'] }})">Approve</button>
-                <button class="modal-close px-2 bg-indigo-500 rounded-lg text-white hover:bg-indigo-400 w-24"
-                    @click="$wire.flag({{ $approval['id'] }})">Flag Issue</button>
-            </div>
+        <div class="flex justify-center pt-2 mt-6">
+            <button id={{ $approval[0]['id'] }}
+                class="px-4 bg-transparent p-2 rounded-lg border-2 border-indigo-500 text-indigo-500 hover:bg-gray-100 hover:text-indigo-400 mr-4 w-24"
+                @click="$wire.approve({{ $approval[0]['id'] }})">Approve</button>
+            <button class="modal-close px-2 bg-indigo-500 rounded-lg text-white hover:bg-indigo-400 w-24"
+                @click="$wire.flag({{ $approval[0]['id'] }})">Flag Issue</button>
         </div>
     </div>
-@endforeach
+</div>
