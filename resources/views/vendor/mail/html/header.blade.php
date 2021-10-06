2df111a74901307058457546d017b083ca1b11f3
Matt Weir <n10509020@qut.edu.au>
2021-10-06 13:49:07 +1000

    Email format change

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,13 @@
+<tr>
+<td class="header">
+<a href="{{ $url }}" style="display: inline-block;">
+@if (trim($slot) === 'Laravel')
+<img src="{{ asset('earn.png') }}" />
+  
+@else
+<img src="{{ asset('earn.png') }}" />
+  
+@endif
+</a>
+</td>
+</tr>
